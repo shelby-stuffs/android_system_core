c5c77619fbf269f048c516b80f0027e24b935825
Siddeswar Aluganti <quic_salugant@quicinc.com>
2024-05-28 10:09:05 -0700

    Revert "init: delay 'user root' test to W+"
    This reverts commit bd53e82e21489905035ea36d33852106d9b36b1b.
    
    CRs-Fixed: 3817336
    Change-Id: I852654a8e5fa93da11f9858f09fbc55155d1dbfc

--- init/service_parser.cpp
+++ init/service_parser.cpp
@@ -680,12 +680,12 @@ Result<void> ServiceParser::EndSection() {
     }
 
     if (service_->proc_attr_.parsed_uid == std::nullopt) {
-        if (android::base::GetIntProperty("ro.vendor.api_level", 0) > __ANDROID_API_V__) {
+        if (android::base::GetIntProperty("ro.vendor.api_level", 0) > __ANDROID_API_U__) {
             return Error() << "No user specified for service '" << service_->name()
-                           << "', so it would have been root.";
+                           << "'. Defaults to root.";
         } else {
             LOG(WARNING) << "No user specified for service '" << service_->name()
-                         << "', so it is root.";
+                         << "'. Defaults to root.";
         }
     }
 
